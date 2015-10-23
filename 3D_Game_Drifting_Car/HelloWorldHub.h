43947fde72ca795c936756f64eb890cfb7734f5c
dex36 <patsadow2@gmail.com>
2015-10-24 01:16:10 +0200

    Create HelloWorldHub.h

---
+++ 3D_Game_Drifting_Car/HelloWorldHub.h
@@ -0,0 +1,21 @@
+
+#ifndef __HELLOWORLDHUB_SCENE_H__
+#define __HELLOWORLDHUB_SCENE_H__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+class HelloWorldHud : public cocos2d::Layer
+{
+public:
+	cocos2d::ui::Button *buttonup;
+	cocos2d::ui::Button *buttonleft;
+	cocos2d::ui::Button *buttonright;
+	virtual bool init();
+	bool movingup;
+	bool movingleft;
+	bool movingright;
+	CREATE_FUNC(HelloWorldHud);
+};
+
+#endif
